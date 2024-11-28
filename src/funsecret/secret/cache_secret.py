fda642048795f38fc26038f0805c5314faa6da9c
farfun <1007530194@qq.com>
2024-11-28 19:39:08 +0800

    add

--- src/funsecret/secret/cache_secret.py
+++ src/funsecret/secret/cache_secret.py
@@ -1,6 +1,6 @@
 import base64
+import hashlib
 import os
-import time
 from urllib.parse import quote_plus
 
 from diskcache import Cache
@@ -17,12 +17,11 @@ class CacheSecretManage:
             secret_dir = os.environ.get("FUN_CACHE_SECRET_PATH")
         if secret_dir is None:
             secret_dir = f'{os.environ.get("FUN_CACHE_SECRET_HOME") or os.environ["HOME"]}/.secret_cache'
-        self.cache = Cache(secret_dir=secret_dir)
-
+        self.cache = Cache(directory=secret_dir)
         self.cipher_key = (
             cipher_key
             or base64.urlsafe_b64encode(
-                quote_plus(secret_dir)[:32].encode("utf-8")
+                quote_plus(secret_dir * 2)[:32].encode("utf-8")
             ).decode()
         )
 
@@ -41,6 +40,17 @@ class CacheSecretManage:
         :return:解密后的文本
         """
         return decrypt(encrypted_text, self.cipher_key)
+
+    @staticmethod
+    def _get_key(
+        cate1,
+        cate2,
+        cate3="",
+        cate4="",
+        cate5="",
+    ):
+        key = f"{cate1}-{cate2}-{cate3}-{cate4}-{cate5}"
+        return hashlib.md5(key.encode("utf-8")).hexdigest()
 
     def read(
         self,
@@ -64,44 +74,45 @@ class CacheSecretManage:
         :param value: 保存的数据
         :param save: 是否需要保存，保存的话，会覆盖当前保存的数据
         :param secret: 是否需要加密，如果加密的话，构造类的时候，cipher_key不能为空，这是加密解密的秘钥
-        :param expire_time: 过期时间，unix时间戳，如果小于10000000的话，会当做保存数据的持续时间，加上当前的Unix时间戳作为过期时间
+        :param expire_time: 过期时间,单位为秒
         :return: 保存的数据
         """
-        cache_key = f"{cate1}-{cate2}-{cate3}-{cate4}-{cate5}"
-        cache_value = value
+        cache_key = self._get_key(
+            cate1=cate1, cate2=cate2, cate3=cate3, cate4=cate4, cate5=cate5
+        )
 
-        if secret:
-            cache_key = self.encrypt(cache_key)
-            cache_value = self.encrypt(cache_value) if cache_value else None
-
-        if expire_time is not None and expire_time < 1000000000:
-            expire_time += int(time.time())
-        if save:
+        if save and value is not None:
             self.write(
-                value,
-                cate1,
-                cate2,
-                cate3,
-                cate4,
-                cate5,
+                cate1=cate1,
+                cate2=cate2,
+                cate3=cate3,
+                cate4=cate4,
+                cate5=cate5,
+                value=value,
                 secret=secret,
                 expire_time=expire_time,
             )
         if value is not None:
             return value
+
         value = self.cache.get(cache_key)
-        return self.decrypt(value) if value is not None and secret else value
+        if value is None:
+            logger.warning(
+                f"not found value from '{cate1}/{cate2}/{cate3}/{cate4}/{cate5}'"
+            )
+            return value
+        return self.decrypt(value) if secret else value
 
     def write(
         self,
-        value,
         cate1,
-        cate2="",
+        cate2,
         cate3="",
         cate4="",
         cate5="",
+        value=None,
         secret=True,
-        expire_time=99999999,
+        expire_time=None,
     ):
         """
         对数据进行保存
@@ -115,17 +126,12 @@ class CacheSecretManage:
         :param expire_time:过期时间，默认不过期
         """
         if value is None:
+            logger.error(f"value cannot be None")
             return
-        cache_key = f"{cate1}-{cate2}-{cate3}-{cate4}-{cate5}"
-        cache_value = value
-
-        if secret:
-            cache_key = self.encrypt(cache_key)
-            cache_value = self.encrypt(cache_value) if cache_value else None
-
-        if expire_time is not None and expire_time < 1000000000:
-            expire_time += int(time.time())
-
+        cache_key = self._get_key(
+            cate1=cate1, cate2=cate2, cate3=cate3, cate4=cate4, cate5=cate5
+        )
+        cache_value = self.encrypt(value) if secret else value
         self.cache.set(cache_key, cache_value, expire=expire_time)
 
 
@@ -138,7 +144,7 @@ def read_cache_secret(
     value=None,
     save=True,
     secret=True,
-    expire_time=9999999,
+    expire_time=None,
 ):
     manage = CacheSecretManage()
     value = manage.read(
@@ -152,10 +158,6 @@ def read_cache_secret(
         secret=secret,
         expire_time=expire_time,
     )
-    if value is None:
-        logger.warning(
-            f"not found value from '{cate1}/{cate2}/{cate3}/{cate4}/{cate5}'"
-        )
     return value
 
 
@@ -167,7 +169,7 @@ def write_cache_secret(
     cate4="",
     cate5="",
     secret=True,
-    expire_time=9999999,
+    expire_time=None,
 ):
     manage = CacheSecretManage()
     manage.write(
